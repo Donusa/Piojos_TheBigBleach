a7120ce1ec064c95ec77fdc5a86f86a0260b74c2
Luca D'Onofrio <lucadonofrio097@gmail.com>
2022-09-11 16:16:27 -0300

    Cambios a la estructura del playerController y movimiento de camara

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float horizontalInput;
     [SerializeField] private float verticalInput;
     [SerializeField] private float speed;
+    [SerializeField] private float rotationSpeed = .3f;
     [SerializeField] private float jumpForce;
+    [SerializeField] private short jumpsLeft;
     [SerializeField] private short maxJumps = 1;
+    [SerializeField] private CharacterController characterController;
 
-    private short jumpsLeft;
     private Vector3 jump;
     private Rigidbody rb;
 
+
+    public Camera cam;
+
+
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+       // rb = GetComponent<Rigidbody>();
         jump = new Vector3(0.0f, jumpForce, 0.0f);
         jumpsLeft = maxJumps;
+        characterController = GetComponent<CharacterController>();
     }
+
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -28,15 +38,47 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        Movement();
+
+        /*
+         * 
+         * 
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
-        if (Input.GetKeyDown(KeyCode.Space) && jumpsLeft>0)
+        if(horizontalInput != 0 || verticalInput != 0)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation,
+                                Quaternion.Euler(0, Camera.main.transform.localEulerAngles.y , 0),
+                                rotationSpeed);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space) && jumpsLeft > 0)
         {
             Jump();
         }
+         */
+
+    }
+
+    private void Movement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 dir = Quaternion.Euler(0, cam.transform.eulerAngles.y, 0) *  new Vector3(horizontalInput, 0.0f, verticalInput);
+        Vector3 dirNorm = dir.normalized;
+
+        if (dirNorm != Vector3.zero)
+        {
+            Quaternion rot = Quaternion.LookRotation(dirNorm, Vector3.up);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                   rot, rotationSpeed * Time.deltaTime);
+        }
+
+
+        characterController.Move(dirNorm * speed * Time.deltaTime);
     }
 
     void Jump()
